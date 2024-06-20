09c3b9bab0de3de115acd101607dec9c01646dc5
dcpais <dpaismail@gmail.com>
2024-06-21 00:37:15 +1000

    Recommit

--- Assets/Scripts/NetworkManagerUI.cs
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,5 +11,19 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] private Button _serverButton;
     [SerializeField] private Button _hostButton;
     [SerializeField] private Button _clientButton;
-    
+
+    private void Awake() {
+        _serverButton.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartServer();
+        });
+        _hostButton.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartHost();
+        });
+        _clientButton.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartClient();
+        });
+        Console.Write("Hello");
+
+    }
+
 }
