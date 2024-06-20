0f742ecc813b59a705362aba880f119646806aac
dcpais <dpaismail@gmail.com>
2024-06-21 00:06:40 +1000

    Added network manager UI, yet to implement functionality

---
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private Button _serverButton;
+    [SerializeField] private Button _hostButton;
+    [SerializeField] private Button _clientButton;
+    
+}
